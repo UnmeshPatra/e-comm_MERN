23dcf242c5aa1d38343be5c487fe6c29fce72c6c
Unmesh <unmeshpatra2004@gmail.com>
2025-01-24 16:53:07 +0530

    mongo

---
+++ config/mongooes-connect.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+module.exports = mongoose.connection;
